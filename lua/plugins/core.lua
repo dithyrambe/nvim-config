34b8cf82c347fb5fdbef1ca00ae5513dc2ee09da
lvpasquier <louis-victor.pasquier@adevinta.com>
2024-11-27 11:02:43 +0100

    :brain: Setup pyright LSP

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -42,4 +42,33 @@ return {
       end)
     end,
   },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+      require("mason").setup()
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup({
+        ensure_installed = { "pyright" },
+      })
+      require("lspconfig").pyright.setup({
+        capabilities = capabilities,
+      })
+    end,
+    keys = {
+      {
+        "gd",
+        function()
+          vim.lsp.buf.declaration()
+        end,
+        desc = "Go to declaration",
+      },
+    },
+  },
 }
