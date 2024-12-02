6a9a8f3a5a7ccd820e885b018cf08ecb139524b4
lvpasquier <louis-victor.pasquier@adevinta.com>
2024-12-02 23:14:35 +0100

    Add neotest configuration

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-neotest/neotest",
+  optional = true,
+  dependencies = {
+    "nvim-neotest/neotest-python",
+  },
+  opts = {
+    adapters = {
+      ["neotest-python"] = {
+        runner = "pytest",
+        args = { "-s" },
+        python = ".venv/bin/python",
+      },
+    },
+  },
+}
